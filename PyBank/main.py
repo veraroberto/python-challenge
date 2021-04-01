facbac89206a3ebe1eadbe6567bbabd0c1914753
Roberto Vera Cuellar <robertovera@192.168.1.21>
2021-04-01 08:24:37 -0600

    python-challenge

---
+++ PyBank/main.py
@@ -0,0 +1,73 @@
+import os
+import csv
+
+resources_file = os.path.join("Resources","budget_data.csv")
+
+
+#Initiate Variables
+count_months = 0
+total_profit = 0
+sum_changes = 0
+sum_dif = 0
+greates_increase = 0
+month_increase = ""
+greates_decrease = 0 
+month_decrease = ""
+
+# se tiene que checar como accesar el dato de otra forma
+previous_month = 867884
+
+with open(resources_file) as csvfile:
+   csv_reader = csv.reader(csvfile, delimiter=",")
+   #HEADER
+   csv_header = next(csvfile)
+   print(csv_header)
+
+   for row in csv_reader:
+       
+       total_profit = total_profit + float(row[1])
+       count_months += 1
+       sum_changes = sum_changes + float(row[1])
+       
+
+       #Calculate the Difference of Profit/loss and the Average
+       dif = float(row[1]) - previous_month
+       sum_dif = sum_dif + dif
+
+       if dif >= greates_increase:
+           greates_increase = dif
+           month_increase = row[0]
+
+       if dif <= greates_decrease:
+           greates_decrease = dif
+           month_decrease = row[0]
+        
+       previous_month = float(row[1])
+       
+       
+average_changes = sum_dif /(count_months - 1)
+
+print("Financial Analysis")
+print("---------------------------------------------")
+print(f'Total Months: {count_months}')
+print(f'Total ${total_profit}')
+print(f'Average Change: {average_changes}')
+print(f'Greatest Increase in Profits:  {month_increase} {greates_increase}')
+print(f'Greatest Decrease in Profits: {month_decrease} {greates_decrease}')
+
+#Write file
+output_path = os.path.join("Analysis","Results.csv")
+
+
+with open(output_path, 'w', newline='') as csvfile:
+
+    # Initialize csv.writer     , delimiter=','
+    csvwriter = csv.writer(csvfile)
+
+    # Write the first row (column headers)
+    csvwriter.writerow(['Financial Analysis'])
+    csvwriter.writerow(['----------------------------'])
+    csvwriter.writerow([f'Total Months: {count_months}'])
+    csvwriter.writerow([f'Average Change: {average_changes}'])
+    csvwriter.writerow([f'Greatest Increase in Profits: {month_increase} {greates_increase}'])
+    csvwriter.writerow([f'Greatest Decrease in Profits: {month_decrease} {greates_decrease}'])
---
+++ PyBoss/main.py
@@ -0,0 +1,76 @@
+import os
+import csv
+
+resources_file = os.path.join("employee_data.csv")
+
+us_state_abbrev = {
+    'Alabama': 'AL',
+    'Alaska': 'AK',
+    'Arizona': 'AZ',
+    'Arkansas': 'AR',
+    'California': 'CA',
+    'Colorado': 'CO',
+    'Connecticut': 'CT',
+    'Delaware': 'DE',
+    'Florida': 'FL',
+    'Georgia': 'GA',
+    'Hawaii': 'HI',
+    'Idaho': 'ID',
+    'Illinois': 'IL',
+    'Indiana': 'IN',
+    'Iowa': 'IA',
+    'Kansas': 'KS',
+    'Kentucky': 'KY',
+    'Louisiana': 'LA',
+    'Maine': 'ME',
+    'Maryland': 'MD',
+    'Massachusetts': 'MA',
+    'Michigan': 'MI',
+    'Minnesota': 'MN',
+    'Mississippi': 'MS',
+    'Missouri': 'MO',
+    'Montana': 'MT',
+    'Nebraska': 'NE',
+    'Nevada': 'NV',
+    'New Hampshire': 'NH',
+    'New Jersey': 'NJ',
+    'New Mexico': 'NM',
+    'New York': 'NY',
+    'North Carolina': 'NC',
+    'North Dakota': 'ND',
+    'Ohio': 'OH',
+    'Oklahoma': 'OK',
+    'Oregon': 'OR',
+    'Pennsylvania': 'PA',
+    'Rhode Island': 'RI',
+    'South Carolina': 'SC',
+    'South Dakota': 'SD',
+    'Tennessee': 'TN',
+    'Texas': 'TX',
+    'Utah': 'UT',
+    'Vermont': 'VT',
+    'Virginia': 'VA',
+    'Washington': 'WA',
+    'West Virginia': 'WV',
+    'Wisconsin': 'WI',
+    'Wyoming': 'WY',
+}
+
+
+with open(resources_file) as csvfile:
+   csv_reader = csv.reader(csvfile, delimiter=",")
+   #HEADER
+   csv_header = next(csvfile)
+   print(csv_header)
+
+   
+   for row in csv_reader:
+       #Split the names
+       split_colum = row[1].split(" ")
+       row[1] = split_colum[0]
+       row.insert(2,split_colum[1])
+
+       #Date
+       split_date = row[3].split("-")
+       print(row)
+       

5ffbf7dac78291d4242b641f9a599b6dadf101b2
Roberto Vera Cuellar <robertovera@192.168.1.21>
2021-04-02 12:59:24 -0600

    python-challenge

--- PyBoss/main.py
+++ PyBoss/main.py
@@ -2,6 +2,7 @@ import os
 import csv
 
 resources_file = os.path.join("employee_data.csv")
+output_path = os.path.join("employee_data_converted.csv")
 
 us_state_abbrev = {
     'Alabama': 'AL',
@@ -56,21 +57,42 @@ us_state_abbrev = {
     'Wyoming': 'WY',
 }
 
+with open(output_path, 'w', newline='') as csvfile:
+    csvwriter = csv.writer(csvfile, delimiter=',')
+    # Write the first row (column headers)
+    csvwriter.writerow(['Emp ID','First Name','Last Name','DOB','SSN','State'])
 
-with open(resources_file) as csvfile:
-   csv_reader = csv.reader(csvfile, delimiter=",")
-   #HEADER
-   csv_header = next(csvfile)
-   print(csv_header)
+    with open(resources_file) as csvfile:
+        csv_reader = csv.reader(csvfile, delimiter=",")
+        #HEADER
+        csv_header = next(csvfile)
+        #print(csv_header)
 
-   
-   for row in csv_reader:
-       #Split the names
-       split_colum = row[1].split(" ")
-       row[1] = split_colum[0]
-       row.insert(2,split_colum[1])
 
-       #Date
-       split_date = row[3].split("-")
-       print(row)
-       
+        for row in csv_reader:
+            #Split the names
+            split_colum = row[1].split(" ")
+            row[1] = split_colum[0]
+            row.insert(2,split_colum[1])
+
+            #Date
+            split_date = row[3].split("-")
+            #Before YYYY-MM-DD
+            row[3] = split_date[1] + "/" + split_date[2] + "/" + split_date[0]    
+            
+            #SNN
+            split_SNN = row[4].split("-")
+            row[4] = "***-**-" + split_SNN[2]
+            
+            #State
+            row[5] = us_state_abbrev[row[5]]
+
+            #Write the File
+            csvwriter.writerow(row)
+
+
+
+    
+        
+    
+    
